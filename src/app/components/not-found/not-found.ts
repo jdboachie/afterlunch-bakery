846f6a9cc44adfd7213f75b189d79515cdcc2275
Jude Boachie <jude.boachie@amalitech.com>
2026-01-21 09:21:56 +0000

    Add NotFound component and configure routing for 404 errors

---
+++ src/app/components/not-found/not-found.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `
+    <p class="text-lg size-full justify-center items-center divide-x flex">
+      <span class="px-4">404</span>
+      <span class="px-4">Page Not Found</span>
+    </p>`
+})
+export class NotFound {}
