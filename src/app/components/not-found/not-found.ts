80b14dd9e0b657825fe2a5c3b4f03be6a58a5f10
Jude Boachie <jude.boachie@amalitech.com>
2026-01-26 11:56:34 +0000

    Remove ChangeDetectionStrategy and OnInit impl

--- src/app/components/not-found/not-found.ts
+++ src/app/components/not-found/not-found.ts
@@ -1,10 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
 import { Logging } from '../../services/logging';
 
 @Component({
   selector: 'app-not-found',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
   template: ` <div class="flex justify-center items-center min-h-[calc(100dvh-200px)]">
     <p class="text-sm divide-x">
       <span class="px-4">404</span>
@@ -12,10 +10,10 @@ import { Logging } from '../../services/logging';
     </p>
   </div>`,
 })
-export class NotFound implements OnInit {
+export class NotFound {
   private readonly logging = inject(Logging);
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.logging.reportError('User hit a 404 page');
   }
 }
